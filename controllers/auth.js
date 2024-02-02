cd1d5766c7200ee0420d7945534270789b7d743a
florinfilip <florin_filip30@yahoo.com>
2024-02-02 11:49:23 +0200

    Added the auth controller and route and the login page

---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('65ba30d34b5bd6c2e51d388e')
+    .then(user => {
+      console.log(user);
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
